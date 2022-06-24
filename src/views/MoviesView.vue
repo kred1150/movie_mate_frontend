7037a6fa37947607ecd9b126433298682dd4fe4f
Kevin Redmond <kred1150@gmail.com>
2022-06-23 20:36:32 -0500

    Added a MvoiesView paage that shows an index of all the movies in the rails database and displays it on the page as raw json. Added the route and path in index.js as well.

---
+++ src/views/MoviesView.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/movies").then((response) => {
+        console.log("Index Movies", response);
+        this.movies = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="movies">
+    <h1>{{ movies }}</h1>
+  </div>
+</template>
+
+<style></style>
