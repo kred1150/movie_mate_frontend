d12f5f8fcabcef5d6ab5dbc5d27c99a1f5e8727e
Kevin Redmond <kred1150@gmail.com>
2023-05-01 17:15:56 -0500

    Added the showActors method to the ActorsShow view.

--- src/views/ActorsShow.vue
+++ src/views/ActorsShow.vue
@@ -1,18 +1,42 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      isLoggedIn: false,
+      bio: [],
+      birthday: [],
+      deathday: [],
+      name: [],
+      birthplace: [],
+      profile_path: [],
+      cinemetography: [],
     };
   },
-  created: function () {},
-  methods: {},
+  // created: function () {
+  //   this.isLoggedIn = localStorage.user_id;
+  //   this.showMovie();
+  // },
+  methods: {
+    showActor: function () {
+      axios.get("/actors/" + this.$route.params.id).then((response) => {
+        console.log("Show Actor", response);
+        this.bio = response.data["bio"];
+        this.birthday = response.data["birthday"];
+        this.deathday = response.data["deathday"];
+        this.name = response.data["name"];
+        this.birthplace = response.data["birthplace"];
+        this.profile_path = response.data["profile_path"];
+        this.cinemetography = response.data["cinemetography"];
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>Actor: {{ this.name }}</h1>
   </div>
 </template>
 
